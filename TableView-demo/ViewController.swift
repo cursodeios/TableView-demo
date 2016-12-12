7e9c9baed5c042ff0781a41d11636c4ed7f1e125
cursodeios <cursodeios@gmail.com>
2016-12-12 16:57:04 -0600

    Finish part 1

--- TableView-demo/ViewController.swift
+++ TableView-demo/ViewController.swift
@@ -1,18 +1,19 @@
-//
-//  ViewController.swift
-//  TableView-demo
-//
-//  Created by Danny Narvaez on 12/12/16.
-//  Copyright © 2016 eHarvestHub. All rights reserved.
-//
+
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,
+UITableViewDelegate,
+UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +21,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "row \(indexPath.row), section \(indexPath.section)"
+        
+        return cell
+    }
 
 }
 
