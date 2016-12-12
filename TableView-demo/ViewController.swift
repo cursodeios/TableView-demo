ee937753e8bf9b76987ae28aac463c708b6c2bbd
cursodeios <cursodeios@gmail.com>
2016-12-12 17:17:20 -0600

    Finish part 2

--- TableView-demo/ViewController.swift
+++ TableView-demo/ViewController.swift
@@ -2,11 +2,18 @@
 
 import UIKit
 
+struct Post {
+    var title: String
+    var subtitle: String
+}
+
 class ViewController: UIViewController,
 UITableViewDelegate,
 UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    
+    var blogPosts: [Post]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,6 +21,12 @@ UITableViewDataSource {
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        blogPosts = [
+            Post(title: "iOS Introduction", subtitle: "Learn iOS and Swift"),
+            Post(title: "NodeJS Course", subtitle: "Learn Nodejs from scratch"),
+            Post(title: "Python for beginners", subtitle: "Learn Python from zero")
+        ]
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,18 +35,20 @@ UITableViewDataSource {
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return blogPosts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        cell.textLabel?.text = "row \(indexPath.row), section \(indexPath.section)"
+        cell.textLabel?.text = blogPosts[indexPath.row].title
+        
+        cell.detailTextLabel?.text = blogPosts[indexPath.row].subtitle
         
         return cell
     }
